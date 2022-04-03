173d06304ad736e393bfdace7a868f19f2bca219
RAZANAJATOVO A Toavina <razanajatovo.a.toavina@gmail.com>
2022-04-03 22:49:32 +0300

    TP1 OK

---
+++ src/main/java/fr/grin/tpcustomerapplication/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package fr.grin.tpcustomerapplication.entities;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author RAZANAJATOVO A Toavina
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/fr/grin/tpcustomerapplication/config/ConfigJSF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.grin.tpcustomerapplication.config;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * @author RAZANAJATOVO A Toavina
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
